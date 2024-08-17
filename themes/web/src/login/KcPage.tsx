002cc5ff18c5d5d48c01663335d934701d6f1cd6
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-08-17 08:51:32 -0400

    feat(themes/web): eject login page

--- themes/web/src/login/KcPage.tsx
+++ themes/web/src/login/KcPage.tsx
@@ -1,14 +1,16 @@
-import { Suspense, lazy } from "react";
 import type { ClassKey } from "keycloakify/login";
-import type { KcContext } from "./KcContext";
-import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+import { Suspense, lazy } from "react";
+import type { KcContext } from "./KcContext";
+import { useI18n } from "./i18n";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
 
 const doMakeUserConfirmPassword = true;
+
+const Login = lazy(() => import("./pages/Login"));
 
 export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
@@ -19,6 +21,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
